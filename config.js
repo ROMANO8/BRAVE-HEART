4c620ec7976990eb2997c3ad27cbaa40336cec05
ROMANO8 <89291999+ROMANO8@users.noreply.github.com>
2024-01-15 14:41:30 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['994404354619', 'BRAVEHEART', true],
+  ['79586872276', 'Ash', true], 
   [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "994404354619" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['994404354619','79586872276'] 
+global.prems = ['994404354619', '79586872276']
+global.allowed = ['994404354619', '79586872276']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,17 +50,17 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '★𝐁𝐑𝐀𝐕𝐄 - 𝐇𝐄𝐀𝐑𝐓★'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
+global.packname = '★𝐁𝐑𝐀𝐕𝐄 - 𝐇𝐄𝐀𝐑𝐓★' 
+global.author = '@Braveheart' 
+global.menuvid = 'https://i.imgur.com/PV0pF55.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
+global.fgsc = 'https://github.com' 
+global.fgyt = 'https://youtube.com'
+global.fgpyp = 'https://youtube.com'
+global.fglog = 'https://raw.githubusercontent.com/ROMANO8/ROMANO/main/ROM1/admins.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
